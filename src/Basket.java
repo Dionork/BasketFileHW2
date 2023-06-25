f351821e48b29dca054b87b72d4467c635a8f746
Dionork <rodya.ru@inbox.ru>
2023-06-25 12:33:59 +0700

    Заготовка

--- src/Basket.java
+++ src/Basket.java
@@ -1,4 +1,32 @@
 package src;
 
 public class Basket {
+    private String [] goods;
+    private int [] prices;
+    private int[] quantities;
+
+    public Basket() {
+
+    }
+
+    public Basket(String [] goods, int [] prices){
+        this.goods = goods;
+        this.prices = prices;
+        this.quantities = new int[goods.length];
+    }
+
+    public void addToCard(int productNum, int amount) {quantities[productNum] += amount;}
+
+    public void printCart(){
+        int totalPrice = 0;
+        System.out.println("Список покупок:");
+        for (int i=0; i< goods.length; i++){
+            if (quantities[i]>0){
+                int currentPrice = prices[i] * quantities[i];
+                totalPrice += currentPrice;
+                System.out.printf("%15s%4d р/шт%4d шт%6d p%n", goods[i],prices[i],quantities[i], currentPrice);
+            }
+        }
+        System.out.printf("Итого: %dp", totalPrice);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,34 @@
 package src;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static String[] products = {"Молоко","Хлеб","Гречка"};
+    static int [] prices = {78,50,64};
+
+    public static void main(String[] args) {
+
+        Basket basket = new Basket(products,prices);
+
+        while (true){
+            showPrice();
+            System.out.println("Выберите товар и количество через пробел или введите end");
+            String input = scanner.nextLine();
+            if ("end".equals(input)){
+                break;
+            }
+            String [] parts = input.split(" ");
+            int productNumber = Integer.parseInt(parts[0]) - 1;
+            int productCount = Integer.parseInt(parts[1]);
+            basket.addToCard(productNumber,productCount);
+        }
+        basket.printCart();
+    }
+    public static void showPrice(){
+        System.out.println("Список товаров для покупки");
+        for (int i =0; i< products.length; i++){
+            System.out.println(products[i] + " " + prices[i] + "руб/шт");
+        }
+    }
 }
